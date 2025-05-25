b398cac75489b6d08be8b97fca03a09aa28b8f93
taggara <58611344+taggara@users.noreply.github.com>
2025-05-24 20:43:34 -0400

    Remove Babel dependencies and update configuration

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['.'],
-          alias: {
-            '@': '.'
-          }
-        }
-      ]
+      'react-native-reanimated/plugin'
     ],
   };
 };
